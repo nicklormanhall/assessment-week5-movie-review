fccb2eb92ca7ce88bd26f810edbe0e324e2a5b0a
sahilavaran <sahil.avaran321@gmail.com>
2024-04-18 10:20:26 +0100

    made_changes

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const movieTitle = document.getElementById("movieTitle").value;
+  const rating = document.getElementById("rating").value;
 
   if (movieTitle.trim() === "") {
     alert("Please enter a movie title.");
     return;
   }
+
+  rating = Math.round((rating / 100) * 100);
+
+  console.log("Movie Title:", movieTitle);
+  console.log("Rating:", rating);
 
   console.log("Form submitted successfully!");
 });
