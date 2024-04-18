46daa4f1a337abbf268fcaa2ba54593c77fff46e
isaacgomu <IsaacDalglish03@gmail.com>
2024-04-18 13:01:51 +0100

    added functionality to add movies to database from form on second page

--- client/second_page/app.js
+++ client/second_page/app.js
@@ -1,14 +1,37 @@
 const form = document.getElementById("movieReviewForm");
 
-form.addEventListener("submit", function (event) {
+console.log("hello!");
+async function handleSubmit(event) {
   event.preventDefault();
 
   const movieTitle = document.getElementById("movieTitle").value;
+  const genre = document.getElementById("genre").value;
+  const imageUrl = document.getElementById("imageUrl").value;
+  const summary = document.getElementById("summary").value;
+  const rating = document.getElementById("rating").value;
 
   if (movieTitle.trim() === "") {
     alert("Please enter a movie title.");
     return;
   }
 
+  const formValues = {
+    name: movieTitle,
+    genre: genre,
+    imageUrl: imageUrl,
+    summary: summary,
+    rating: rating,
+  };
+
+  const addmovie = await fetch("http://localhost:8080/movieadd", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
   console.log("Form submitted successfully!");
-});
+  form.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
